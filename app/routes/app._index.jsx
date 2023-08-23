62311f2760ac61da8746fead17cf587c42c7824a
fxhanloniii <hanlonfrankiex@gmail.com>
2023-08-23 08:53:11 -0700

    adding in bulk operation

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,43 +1,115 @@
 // Import necessary dependencies
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { Page, Layout, Text, VerticalStack, Card, List } from "@shopify/polaris";
+import { Page, Layout, Text, VerticalStack, Card, List, Select } from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
 
 // Loader function to authenticate and fetch product information
 export const loader = async ({ request }) => {
   const { admin } = await authenticate.admin(request);
 
-  // GraphQL query to fetch the first 10 products
-  const response = await admin.graphql(`
-    {
-      products(first: 10) {
-        edges {
-          node {
-            id
-            title
-            handle
-          }
-        }
-      }
-    }
-  `);
+   // I need a bulk operation query, a webhook to check when operation is done and to display the information on the page
 
-  const responseJson = await response.json();
+   
+  
 
-  return json({
-    products: responseJson.data.products.edges.map(edge => edge.node),
-  });
-};
+
+
+}
 
 // Main component to render the page
 export default function Index() {
-  const { products } = useLoaderData(); // Get the product information from loader data
+  const { data } = useLoaderData(); // Get the product and order information from loader data
+  console.log("data")
+  useEffect(() => {
+    if (data && data.products) {
+      console.log("Product Data:", data.products.edges.node);
+    }
+  }, [data]);
+
+    
+    
+  const [dropdownValue1, setDropdownValue1] = useState("30");
+  const [dropdownValue2, setDropdownValue2] = useState("60");
+  const [dropdownValue3, setDropdownValue3] = useState("90");
+
+  
 
   return (
     <Page>
-      <ui-title-bar title="Product Information"></ui-title-bar>
+      <ui-title-bar title="Inventory Aging Markdown"></ui-title-bar>
+      <Layout.Section>
+        <Card>
+          <VerticalStack gap="5" >
+            <Text as="h2" variant="headingMd">
+              Dropdown 1
+            </Text>
+            <Select
+              label="Select an option"
+              options={[
+                { label: "30 days", value: "30" },
+                { label: "60 days", value: "60" },
+                { label: "90 days", value: "90" },
+                { label: "n.a", value: "n.a" },
+              ]}
+              value={dropdownValue1}
+              onChange={setDropdownValue1}
+            />
+            <Select
+              label="Select an option"
+              options={[
+                { label: "30 days", value: "30" },
+                { label: "60 days", value: "60" },
+                { label: "90 days", value: "90" },
+                { label: "n.a", value: "n.a" },
+              ]}
+              value={dropdownValue1}
+              onChange={setDropdownValue1}
+            />
+          </VerticalStack>
+        </Card>
+      </Layout.Section>
+      {/* <Layout.Section>
+        <Card>
+          <VerticalStack gap="5">
+            <Text as="h2" variant="headingMd">
+              Dropdown 2
+            </Text>
+            <Select
+              label="Select an option"
+              options={[
+                { label: "30 days", value: "30" },
+                { label: "60 days", value: "60" },
+                { label: "90 days", value: "90" },
+                { label: "n.a", value: "n.a" },
+              ]}
+              value={dropdownValue2}
+              onChange={setDropdownValue2}
+            />
+          </VerticalStack>
+        </Card>
+      </Layout.Section>
+      <Layout.Section>
+        <Card>
+          <VerticalStack gap="5">
+            <Text as="h2" variant="headingMd">
+              Dropdown 3
+            </Text>
+            <Select
+              label="Select an option"
+              options={[
+                { label: "30 days", value: "30" },
+                { label: "60 days", value: "60" },
+                { label: "90 days", value: "90" },
+                { label: "n.a", value: "n.a" },
+              ]}
+              value={dropdownValue3}
+              onChange={setDropdownValue3}
+            />
+          </VerticalStack>
+        </Card>
+      </Layout.Section> */}
       <VerticalStack gap="5">
         <Layout>
           <Layout.Section>
@@ -47,11 +119,23 @@ export default function Index() {
                   List of Products
                 </Text>
                 <List spacing="extraTight">
-                  {products.map((product) => (
-                    <List.Item key={product.id}>
-                      {product.title} (Handle: {product.handle})
-                    </List.Item>
-                  ))}
+                
+                </List>
+              </VerticalStack>
+            </Card>
+          </Layout.Section>
+        </Layout>
+      </VerticalStack>
+      <VerticalStack gap="5">
+        <Layout>
+          <Layout.Section>
+            <Card>
+              <VerticalStack gap="5">
+                <Text as="h2" variant="headingMd">
+                  List of Orders
+                </Text>
+                <List spacing="extraTight">
+                
                 </List>
               </VerticalStack>
             </Card>
